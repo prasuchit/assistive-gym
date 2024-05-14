3ae7ba602dc51bf6ed1eb7c4235daf60287545d9
Prasanth Sengadu Suresh <prasuchit@gmail.com>
2024-05-14 16:50:31 -0400

    Added code for fatigued human

--- assistive_gym/envs/TBfeeding.py
+++ assistive_gym/envs/TBfeeding.py
@@ -1,3 +1,4 @@
+import itertools
 from random import random
 import numpy as np
 import pybullet as p
@@ -6,6 +7,12 @@ from .env import AssistiveEnv
 from .agents import furniture
 from .agents.furniture import Furniture
 
+AGENT_TYPES = {
+    0: 'Unfatigued',
+    1: 'Fatigued',
+    2: 'Collaborative',
+    3: 'Super-Collaborative',
+}
 
 class TBFeedingEnv(AssistiveEnv):
     def __init__(self, robot, human):
@@ -14,41 +21,98 @@ class TBFeedingEnv(AssistiveEnv):
         because we removed 3 pose, 4 orient values of human head and added 1 self type value and 1 human type instead. 
         For human, the len went from 19 + len(joints) to 21 + len(joints))
         '''
-        self.n_agents = 2
-        self.agents = ['robot', 'human']
-        self.nAgent_type = 2
-        self.prev_agent_type = {ag_id: 0 for ag_id in self.agents}
-        self.current_energy_level = {ag_id: 10 for ag_id in self.agents}
+        self.agent_names = ['robot', 'human']
+        self.n_agents = len(self.agent_names)
+        self.agent_types = AGENT_TYPES
+        self.nAgent_type = len(self.agent_types)
+        self.prev_agent_type = {'robot': 2, 'human': 0}
+        self.current_energy_level = {'human': 10}
+        self.curr_human_action = None
+        self.belief_prob_counter = 0
         
         super(TBFeedingEnv, self).__init__(robot=robot, human=human, task='feeding', obs_robot_len=(13 + len(robot.controllable_joint_indices) -
                                             (len(robot.wheel_joint_indices) if robot.mobile else 0)), obs_human_len=(21 + len(human.controllable_joint_indices)))
 
-    def get_other_agents_types(self, curr_ag_id):
+    def human_preferences(self, end_effector_velocity=0, total_force_on_human=0, tool_force_at_target=0, food_hit_human_reward=0, food_mouth_velocities=None, dressing_forces=None, arm_manipulation_tool_forces_on_human=None):
+        
+        if food_mouth_velocities is None:
+            food_mouth_velocities = []
+        if dressing_forces is None:
+            dressing_forces = [[]]
+        if arm_manipulation_tool_forces_on_human is None:
+            arm_manipulation_tool_forces_on_human = [0, 0]
+        
+        # Slow end effector velocities
+        reward_velocity = -end_effector_velocity
+
+        # < 10 N force at target
+        reward_high_target_forces = 0 if tool_force_at_target < 10 else -tool_force_at_target
+
+        fatigued_human_action_penalty = 0
+        # --- Scooping, Feeding, Drinking ---
+        if self.task in ['feeding', 'drinking']:
+            if self.prev_agent_type['human'] == 1:  # Human is fatigued
+                action = np.copy(self.curr_human_action)
+                fatigued_human_action_penalty = -np.mean(action)
+            # Penalty when robot's body applies force onto a person
+            reward_force_nontarget = -total_force_on_human
+        # Penalty when robot spills food on the person
+        reward_food_hit_human = food_hit_human_reward
+        # Human prefers food entering mouth at low velocities
+        reward_food_velocities = 0 if len(food_mouth_velocities) == 0 else -np.sum(food_mouth_velocities)
+
+        return self.C_v*reward_velocity + self.C_f*reward_force_nontarget + self.C_hf*reward_high_target_forces + \
+                self.C_fd*reward_food_hit_human + self.C_fdv*reward_food_velocities + fatigued_human_action_penalty
+
+
+    def get_other_agents_types_ground_truth(self, curr_ag_id):
         '''
         @brief Given a particular agent id, returns a list of all the other agents' true type.
         '''
         return [
             self.prev_agent_type[ag_id]
-            for ag_id in self.agents
+            for ag_id in self.agent_names
             if ag_id != curr_ag_id
+        ]
+    
+            
+    def get_other_agents_types_for_belief_training(self, curr_ag_id):
+        '''
+        @brief Given a particular agent id, returns a list of all the other agents' probable type.
+        This method is only used to train the belief network and make it learn the belief update distribution.
+        This is not used during policy training or execution.
+        '''
+        # If human is asking for robot's type, always return groundtruth; if robot is asking for human's type
+        # after a fatigued human has done 5 NoOp actions, always return groundtruth; otherwise return the type
+        # fatigued or not with certain probability.
+        return [
+            self.prev_agent_type[ag_id] if ag_id == 0 or self.belief_prob_counter >= 5
+            else random.choices([0, 1], weights=[(5 - self.belief_prob_counter) / 5, self.belief_prob_counter / 5], k=1)[0]
+            for ag_id in self.agent_names if ag_id != curr_ag_id
         ]
         
     def update_types(self):
         '''
-        @brief Updates energy levels of agents and if an agent's energy is 0 then updates their type to fatigued.
-        In this case only the human fatigues.
+        @brief Updates energy levels of agents and if an agent's energy is 0 then updates their type.
         '''
-        self.current_energy_level[self.agents[1]] -= 1 if self.current_energy_level[self.agents[1]] > 0 else self.current_energy_level[self.agents[1]]
-        if self.current_energy_level[self.agents[1]] == 0:
-            self.prev_agent_type[self.agents[1]] = 1
+        # Keep reducing human energy level at every step
+        self.current_energy_level[self.agent_names[1]] -= 1 if self.current_energy_level[self.agent_names[1]] > 0 else self.current_energy_level[self.agent_names[1]]
+        # If human energy level is totally depleted, change his type to fatigued
+        if self.current_energy_level[self.agent_names[1]] == 0:
+            self.prev_agent_type[self.agent_names[1]] = 1
+        # If human is fatigued, robot becomes super-collaborative
+        if self.prev_agent_type[self.agent_names[1]] == 1 and self.prev_agent_type[self.agent_names[0]] == 2:
+            self.prev_agent_type[self.agent_names[0]] = 3
 
     def step(self, action):
         if self.human.controllable:
+            self.curr_human_action = action['human']
             action = np.concatenate([action['robot'], action['human']])
             
         self.update_types()
         self.take_step(action)
-
+        self.belief_prob_counter += 1
+        
         obs = self._get_obs()
 
         reward_food, food_mouth_velocities, food_hit_human_reward = self.get_food_rewards()
@@ -64,6 +128,7 @@ class TBFeedingEnv(AssistiveEnv):
         # Penalize robot for distance between the spoon and human mouth.
         reward_distance_mouth_target = - \
             np.linalg.norm(self.target_pos - spoon_pos)
+            
         reward_action = -np.linalg.norm(action)  # Penalize actions
 
         reward = self.config('distance_weight')*reward_distance_mouth_target + self.config(
@@ -109,8 +174,7 @@ class TBFeedingEnv(AssistiveEnv):
                 food_mouth_velocities.append(food_velocity)
                 foods_to_remove.append(f)
                 foods_active_to_remove.append(f)
-                f.set_base_pos_orient(self.np_random.uniform(
-                    1000, 2000, size=3), [0, 0, 0, 1])
+                f.set_base_pos_orient(self.np_random.uniform(1000, 2000, size=3), [0, 0, 0, 1])
                 continue
             elif len(f.get_closest_points(self.tool, distance=0.1)[-1]) == 0:
                 # Delete particle and give robot a penalty for spilling food
@@ -141,14 +205,13 @@ class TBFeedingEnv(AssistiveEnv):
             robot_joint_angles = robot_joint_angles[len(
                 self.robot.wheel_joint_indices):]
         head_pos, head_orient = self.human.get_pos_orient(self.human.head)
-        head_pos_real, head_orient_real = self.robot.convert_to_realworld(
-            head_pos, head_orient)
+        # head_pos_real, head_orient_real = self.robot.convert_to_realworld(head_pos, head_orient)
         target_pos_real, _ = self.robot.convert_to_realworld(self.target_pos)
         self.robot_force_on_human, self.spoon_force_on_human = self.get_total_force()
         self.total_force_on_human = self.robot_force_on_human + self.spoon_force_on_human
         # NOTE: PS: Removed head_pos_real, head_orient_real from robot obs to let human type provide the info that he's fatigued
         robot_obs = np.concatenate([spoon_pos_real, spoon_orient_real, spoon_pos_real - target_pos_real,
-                                    robot_joint_angles, [self.spoon_force_on_human], [self.prev_agent_type[self.agents[0]]]]).ravel()
+                                    robot_joint_angles, [self.spoon_force_on_human], [self.prev_agent_type[self.agent_names[0]]]]).ravel()
         if agent == 'robot':
             return robot_obs
         if self.human.controllable:
@@ -161,7 +224,7 @@ class TBFeedingEnv(AssistiveEnv):
             target_pos_human, _ = self.human.convert_to_realworld(
                 self.target_pos)
             human_obs = np.concatenate([spoon_pos_human, spoon_orient_human, spoon_pos_human - target_pos_human, human_joint_angles,
-                                        head_pos_human, head_orient_human, [self.robot_force_on_human, self.spoon_force_on_human], [self.prev_agent_type[self.agents[1]]]]).ravel()
+                                        head_pos_human, head_orient_human, [self.robot_force_on_human, self.spoon_force_on_human], [self.prev_agent_type[self.agent_names[1]]]]).ravel()
             if agent == 'human':
                 return human_obs
             # Co-optimization with both human and robot controllable
@@ -200,7 +263,7 @@ class TBFeedingEnv(AssistiveEnv):
                         self.id, self.np_random, right=True, mesh_scale=[0.08]*3)
 
         target_ee_pos = np.array([-0.15, -0.65, 1.15]) + \
-            self.np_random.uniform(-0.05, 0.05, size=3)
+                self.np_random.uniform(-0.05, 0.05, size=3)
         target_ee_orient = self.get_quaternion(
             self.robot.toc_ee_orient_rpy[self.task])
         self.init_robot_pose(target_ee_pos, target_ee_orient, [(target_ee_pos, target_ee_orient), (self.target_pos, None)], [(
@@ -225,12 +288,17 @@ class TBFeedingEnv(AssistiveEnv):
         spoon_pos, spoon_orient = self.tool.get_base_pos_orient()
         food_radius = 0.005
         food_mass = 0.001
-        batch_positions = []
-        for i in range(2):
-            for j in range(2):
-                for k in range(2):
-                    batch_positions.append(np.array(
-                        [i*2*food_radius-0.005, j*2*food_radius, k*2*food_radius+0.01]) + spoon_pos)
+        batch_positions = [
+            np.array(
+                [
+                    i * 2 * food_radius - 0.005,
+                    j * 2 * food_radius,
+                    k * 2 * food_radius + 0.01,
+                ]
+            )
+            + spoon_pos
+            for i, j, k in itertools.product(range(2), range(2), range(2))
+        ]
         self.foods = self.create_spheres(
             radius=food_radius, mass=food_mass, batch_positions=batch_positions, visual=False, collision=True)
         colors = [[60./256., 186./256., 84./256., 1], [244./256., 194./256., 13./256., 1],
@@ -239,7 +307,7 @@ class TBFeedingEnv(AssistiveEnv):
             p.changeVisualShape(
                 f.body, -1, rgbaColor=colors[i % len(colors)], physicsClientId=self.id)
         self.total_food_count = len(self.foods)
-        self.foods_active = [f for f in self.foods]
+        self.foods_active = list(self.foods)
 
         # Enable rendering
         p.configureDebugVisualizer(
@@ -251,9 +319,11 @@ class TBFeedingEnv(AssistiveEnv):
 
         self.init_env_variables()
 
-        self.prev_agent_type = {ag_id: 0 for ag_id in self.agents}
-        self.current_energy_level = {ag_id: 10 for ag_id in self.agents}
+        self.prev_agent_type = {'robot': 2,  'human': 0}
+        self.current_energy_level = {'human': 10}
         self.time_elapsed = 0
+        self.curr_human_action = None
+        self.belief_prob_counter = 0
 
         return self._get_obs()
 
